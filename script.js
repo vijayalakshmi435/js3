d91875ac1f471575543eba0c0916e7b9219f2455
vijayalakshmi <mamillavijayalakshmi789@gmail.com>
2022-11-29 11:04:05 +0530

    ok

---
+++ script.js
@@ -0,0 +1,19 @@
+const btn=()=>{
+
+    document.getElementById('pmt').style.display="block"
+
+}
+
+const after=()=>{
+    document.getElementById('btn2').style.display="none"
+}
+
+const back=()=>{
+    document.getElementById('btn3').style.display="none"
+    document.getElementById('pmt').style.display="none"
+    document.getElementById('in').style.display="none"
+}
+
+const opn=()=>{
+    document.getElementById('in').style.display="flex"
+}
